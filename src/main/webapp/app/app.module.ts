1e169c1cd498b0912de42d4ad474a99dfa9b506f
Mojtaba <mojtaba98.mz@gmail.com>
2022-01-09 11:45:54 +0330

    search box and dev

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -17,6 +17,7 @@ import { SharedModule } from 'app/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { EntityRoutingModule } from './entities/entity-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { NgbDateDayjsAdapter } from './config/datepicker-adapter';
 import { fontAwesomeIcons } from './config/font-awesome-icons';
@@ -33,6 +34,7 @@ import { ErrorComponent } from './layouts/error/error.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     SharedModule,
     HomeModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
--- src/main/webapp/app/entities/register-donation/register-donation.module.ts
+++ src/main/webapp/app/entities/register-donation/register-donation.module.ts
@@ -3,10 +3,23 @@ import { SharedModule } from 'app/shared/shared.module';
 import { RegisterDonationUpdateComponent } from './update/register-donation-update.component';
 import { RegisterDonationRoutingModule } from './route/register-donation-routing.module';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { DxButtonModule } from 'devextreme-angular';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [SharedModule, RegisterDonationRoutingModule, MatSelectModule, MatIconModule],
+  imports: [
+    SharedModule,
+    RegisterDonationRoutingModule,
+    CommonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    NgxMatSelectSearchModule,
+    DxButtonModule,
+  ],
   declarations: [RegisterDonationUpdateComponent],
   entryComponents: [],
 })
